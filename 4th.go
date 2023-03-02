b71a332a6ba86c07bcf252f692418e1114fa2793
Katerina Braide <amkaterinabraide@gmail.com>
2023-03-02 20:58:10 +0100

    update

---
+++ 4th.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+    "fmt"
+    "net"
+    "os"
+)
+
+func main() {
+    // Get the command line arguments
+    args := os.Args[1:]
+
+    // Check if any arguments were provided
+    if len(args) == 0 {
+        fmt.Println("Please provide a command")
+        return
+    }
+
+    // Get the command
+    command := args[0]
+
+    // Switch based on the command
+    switch command {
+    case "ping":
+        // Code to ping a host
+        if len(args) < 2 {
+            fmt.Println("Please provide a host to ping")
+            return
+        }
+        host := args[1]
+        conn, err := net.Dial("ip4:icmp", host)
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        defer conn.Close()
+        fmt.Printf("Pinging %s...\n", host)
+    case "traceroute":
+        // Code to trace the route to a host
+        if len(args) < 2 {
+            fmt.Println("Please provide a host to trace")
+            return
+        }
+        host := args[1]
+        fmt.Printf("Tracing route to %s...\n", host)
+        addrs, err := net.LookupIP(host)
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        for _, addr := range addrs {
+            fmt.Println(addr)
+        }
+    default:
+        // Invalid command
+        fmt.Printf("Invalid command: %s\n", command)
+    }
+}
+//Go program that can be used to develop network tools
+//understand and modify
---
+++ 5th.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func main() {
+    // Define a handler function for the root path
+    rootHandler := func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Welcome to my website!")
+    }
+
+    // Define a handler function for the "/about" path
+    aboutHandler := func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "About me!")
+    }
+
+    // Register the handlers
+    http.HandleFunc("/", rootHandler)
+    http.HandleFunc("/about", aboutHandler)
+
+    // Start the server
+    fmt.Println("Starting server on port 8080")
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+//Go program that can be used for web development
+//understand and modify
