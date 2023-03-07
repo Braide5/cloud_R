d07e98db7a94519fca73f44d68eeee6ced146088
Katerina Braide <amkaterinabraide@gmail.com>
2023-03-07 23:05:07 +0100

    downgrade

--- cloud_dev.go
+++ cloud_dev.go
@@ -19,19 +19,19 @@ func main() {
     command := args[0]
 
     // Switch based on the command
-    switch command {
-    case "deploy":
+    ///switch command {
+    ///case "deploy":
         // Code to deploy to cloud
-        fmt.Println("Deploying to cloud...")
-    case "scale":
+       /// fmt.Println("Deploying to cloud...")
+///    case "scale":
         // Code to scale cloud resources
-        fmt.Println("Scaling cloud resources...")
-    case "monitor":
+   ///     fmt.Println("Scaling cloud resources...")
+    ///case "monitor":
         // Code to monitor cloud resources
-        fmt.Println("Monitoring cloud resources...")
-    default:
+        ///fmt.Println("Monitoring cloud resources...")
+    ///default:
         // Invalid command
-        fmt.Printf("Invalid command: %s\n", command)
+       /// fmt.Printf("Invalid command: %s\n", command)
     }
 }
 //Go program that can be used to develop cloud tools
