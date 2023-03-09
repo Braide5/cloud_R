bd59ef7d4f7b14e6d8509278c34124bf18221336
Katerina Braide <amkaterinabraide@gmail.com>
2023-03-09 21:09:44 +0100

    9/3/2023

--- cloudW_dev.go
+++ cloudW_dev.go
@@ -3,13 +3,22 @@ package main
 import (
     "fmt"
     "net/http"
+    "math/rand"
+    "time"
 )
 
 func main() {
     // Define a handler function for the root path
     rootHandler := func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Welcome to my website!")
+        // Set the seed value for the random number generator
+    rand.Seed(time.Now().UnixNano())
+
+    // Generate and print 10 random integers between 0 and 99
+    for i := 0; i < 10; i++ {
+        fmt.Println(rand.Intn(100))
     }
+}
 
     // Define a handler function for the "/about" path
     aboutHandler := func(w http.ResponseWriter, r *http.Request) {
@@ -38,3 +47,4 @@ func main() {
 //Go program that can be used for web development
 //understand and modify
 //please provide a command
+//this modified one givees output on the terminal
